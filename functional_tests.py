0cea52c905f6ab9653fbe5d51f0d797e229f4f4c
NoahLE <noah@noahle.com>
2021-04-10 12:26:50 -0500

    Chapter 5 code

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Limmy has heard about a cool new online to-do app.
         # He goes to check out its homepage
@@ -25,24 +30,28 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # He types "Buy television" into a text box (Limmy's hobby is reviewing movies)
-        inputbox.send_keys('Buy telivision')
+        inputbox.send_keys('Buy television')
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy television" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1. Buy television' for row in rows), "New to-do item did not appear in table")
+        self.check_for_row_in_list_table('1: Buy television')
 
         # There is still a text box inviting her to add another item.
         # He enter "Use television to leave movie reviews"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use television to leave movie reviews')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on his list
+        self.check_for_row_in_list_table('1: Buy television')
+        self.check_for_row_in_list_table('2: Use television to leave movie reviews')
 
         # Limmy wonders if the website will remember his list.
         # Then he sees the website generated a unique URL - his to-do list is still there
+        self.fail('Finish the test!')
 
         # Satisfied, he goes back to sleep
 
