010fc76a07f1b9d6d6606262386ef3abaea64ab3
NoahLE <noah@noahle.com>
2021-04-17 11:59:16 -0500

    Chapter 6 code

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,24 +1,37 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Limmy has heard about a cool new online to-do app.
         # He goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page titles and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -35,25 +48,20 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy television" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy television')
+        self.wait_for_row_in_list_table('1: Buy television')
 
         # There is still a text box inviting her to add another item.
         # He enter "Use television to leave movie reviews"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use television to leave movie reviews')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on his list
-        self.check_for_row_in_list_table('1: Buy television')
-        self.check_for_row_in_list_table('2: Use television to leave movie reviews')
+        self.wait_for_row_in_list_table('1: Buy television')
+        self.wait_for_row_in_list_table('2: Use television to leave movie reviews')
 
         # Limmy wonders if the website will remember his list.
         # Then he sees the website generated a unique URL - his to-do list is still there
         self.fail('Finish the test!')
 
         # Satisfied, he goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main()
